9baa305c23acb38bf2388f449639b31cfd7ec315
Idalia <idaliamedeiros@hotmail.com>
2023-11-25 14:25:08 -0300

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,10 @@
+def hi(name):
+    print('Olá '+ name + '!')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'Idália']
+for name in girls:
+    hi(name)
+    print('Próxima')
+
+for i in range(1, 6):
+    print(i)
